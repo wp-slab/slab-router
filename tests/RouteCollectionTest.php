2f967ee19f3be2d06d425e88855609e4a82eb4f8
Luke Lanchester <luke@lukelanchester.com>
2015-09-02 17:32:41 +0100

    Test RouteCollection

--- tests/RouteCollectionTest.php
+++ tests/RouteCollectionTest.php
@@ -25,4 +25,75 @@ class RouteCollectionTest extends PHPUnit_Framework_TestCase {
 
 
 
+	/**
+	 * Test adding a route
+	 *
+	 * @return void
+	 * @dataProvider routesProvider
+	 **/
+	public function testAddRoute($method, $path, $handler, $group, $path_out) {
+
+		$collection = new RouteCollection;
+
+		$collection->$method($path, $handler);
+
+		$routes = $collection->getRoutes($group);
+
+		$this->assertEquals([['path' => $path_out, 'handler' => $handler]], $routes);
+
+	}
+
+
+
+	/**
+	 * Test adding multiple routes
+	 *
+	 * @return void
+	 **/
+	public function testAddMultipleRoutes() {
+
+		$collection = new RouteCollection;
+		$routes = $this->routesProvider();
+		$tests = [];
+
+		foreach($routes as $route) {
+			list($method, $path, $handler, $group, $path_out) = $route;
+			$collection->$method($path, $handler);
+			$tests[$group][] = ['path' => $path_out, 'handler' => $handler];
+		}
+
+		foreach($tests as $group => $test) {
+			$this->assertEquals($test, $collection->getRoutes($group));
+		}
+
+	}
+
+
+
+	/**
+	 * Provide routes for testing
+	 *
+	 * @return void
+	 **/
+	public function routesProvider() {
+
+		return [
+			['get', '', 'myHandlerGet1', 'GET', ''],
+			['get', '/', 'myHandlerGet2', 'GET', ''],
+			['get', 'foo', 'myHandlerGet3', 'GET', 'foo'],
+			['get', 'foo/bar', 'myHandlerGet4', 'GET', 'foo/bar'],
+			['get', 'foo/{slug}', 'myHandlerGet5', 'GET', 'foo/{slug}'],
+			['get', 'foo/{slug?}', 'myHandlerGet6', 'GET', 'foo/{slug?}'],
+			['post', 'foo', 'myHandlerPost1', 'POST', 'foo'],
+			['post', 'foo/bar', 'myHandlerP2', 'POST', 'foo/bar'],
+			['put', 'foo/bar', 'myHandlerPut1', 'PUT', 'foo/bar'],
+			['delete', 'foo/bar', 'myHandlerDelete1', 'DELETE', 'foo/bar'],
+			['head', 'foo/bar', 'myHandlerHead1', 'HEAD', 'foo/bar'],
+			['options', 'foo/bar', 'myHandlerOptions1', 'OPTIONS', 'foo/bar'],
+		];
+
+	}
+
+
+
 }
