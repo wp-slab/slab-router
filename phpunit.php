f5380ae766a2682f0720cdacaf0626ef6c93050f
Luke Lanchester <luke@lukelanchester.com>
2015-09-02 17:17:02 +0100

    Fixed autoloader for new slab-core dir

--- phpunit.php
+++ phpunit.php
@@ -2,5 +2,5 @@
 
 include 'vendor/autoload.php';
 
-include 'wp-content/plugins/slab-core/src/Autoloader.php';
+include 'vendor/wp-slab/slab-core/src/Autoloader.php';
 (new Slab\Core\Autoloader)->registerNamespace('Slab\Router', __DIR__ . '/src');
