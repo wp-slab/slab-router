842f11f01fb08fd3790127d86add9f2b1f3cc74c
Luke Lanchester <luke@lukelanchester.com>
2015-09-03 19:17:03 +0100

    Normalized autoloader for Core

--- phpunit.php
+++ phpunit.php
@@ -1,6 +1,8 @@
 <?php
 
 include 'vendor/autoload.php';
-
 include 'vendor/wp-slab/slab-core/src/Autoloader.php';
-(new Slab\Core\Autoloader)->registerNamespace('Slab\Router', __DIR__ . '/src');
+
+$autoloader = new Slab\Core\Autoloader;
+$autoloader->registerNamespace('Slab\Core', 'vendor/wp-slab/slab-core/src');
+$autoloader->registerNamespace('Slab\Router', __DIR__ . '/src');
